187787b1139f603e59c80621e830700eed8ff0e4
DPro <35978720+TerminalVelocityDPro@users.noreply.github.com>
2020-08-25 21:01:37 -0700

    smooth auto scroll added

--- buttonMoves.js
+++ buttonMoves.js
@@ -8,6 +8,74 @@ var secondRoundChoice = "<div class = 'col-2' style='height:430px'></div><div cl
 
 var ques = [];
 
+var smooth_scroll_to = function(element, target, duration) {
+    target = Math.round(target);
+    duration = Math.round(duration);
+    if (duration < 0) {
+        return Promise.reject("bad duration");
+    }
+    if (duration === 0) {
+        element.scrollTop = target;
+        return Promise.resolve();
+    }
+
+    var start_time = Date.now();
+    var end_time = start_time + duration;
+
+    var start_top = element.scrollTop;
+    var distance = target - start_top;
+
+    // based on http://en.wikipedia.org/wiki/Smoothstep
+    var smooth_step = function(start, end, point) {
+        if(point <= start) { return 0; }
+        if(point >= end) { return 1; }
+        var x = (point - start) / (end - start); // interpolation
+        return x*x*(3 - 2*x);
+    }
+
+    return new Promise(function(resolve, reject) {
+        // This is to keep track of where the element's scrollTop is
+        // supposed to be, based on what we're doing
+        var previous_top = element.scrollTop;
+
+        // This is like a think function from a game loop
+        var scroll_frame = function() {
+            if(element.scrollTop != previous_top) {
+                reject("interrupted");
+                return;
+            }
+
+            // set the scrollTop for this frame
+            var now = Date.now();
+            var point = smooth_step(start_time, end_time, now);
+            var frameTop = Math.round(start_top + (distance * point));
+            element.scrollTop = frameTop;
+
+            // check if we're done!
+            if(now >= end_time) {
+                resolve();
+                return;
+            }
+
+            // If we were supposed to scroll but didn't, then we
+            // probably hit the limit, so consider it done; not
+            // interrupted.
+            if(element.scrollTop === previous_top
+                && element.scrollTop !== frameTop) {
+                resolve();
+                return;
+            }
+            previous_top = element.scrollTop;
+
+            // schedule next frame for execution
+            setTimeout(scroll_frame, 0);
+        }
+
+        // boostrap the animation process
+        setTimeout(scroll_frame, 0);
+    });
+}
+
 
 
 function appendNo(num){
@@ -43,6 +111,7 @@ d3.select("body").select("#mainContainer").select("#rowContainingSurveyInfoButto
 
 
 function appendStuff(num){
+
 
 
 ques.push(num);
@@ -490,5 +559,10 @@ d3.select("body").select("#mainContainer").select("#rowContainingSurveyInfoButto
 
 d3.select("body").select("#mainContainer").select("#rowContainingSurveyInfoButtons").select("#survey").selectAll(".speech-bubble").transition().duration(2000).style("opacity", "1");
 d3.select("body").select("#mainContainer").select("#rowContainingSurveyInfoButtons").select("#survey").selectAll(".user-bubble").transition().duration(2000).style("opacity", "1");
+var element = document.getElementById("survey");
+//element.scrollTop = (element.scrollHeight - element.clientHeight) - 100;
+smooth_scroll_to(element, (element.scrollHeight - element.clientHeight), 1500);
+
+
 
 }
